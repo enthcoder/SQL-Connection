dd6671ee496cffbec25dcdb46242ae82f2ea6fa1
enthcoder <tanaka950117@gmail.com>
2018-09-11 09:17:40 -0500

    Update query.js

--- controllers/query.js
+++ controllers/query.js
@@ -15,7 +15,10 @@ exports.getPending = (req, res) => {
     return pool.request().query('select count(*) as \'pending\' from tb_cfdi where sent = \'0\'')
   }).then(result => {
     let rows = result.recordset
-    res.status(200).json(rows)
+    res.status(200).json({
+      title: 'pendientes',
+      quantity: rows[0].pending
+    })
     sql.close();
   }).catch(err => {
     res.status(500).send({message: '${err}'})
@@ -28,7 +31,10 @@ exports.getProcessed = (req, res) => {
     return pool.request().query('select count(*) as \'processed\' from tb_cfdi')
   }).then(result => {
     let rows = result.recordset
-    res.status(200).json(rows)
+    res.status(200).json({
+      title: 'processed',
+      quantity: rows[0].processed
+    })
     sql.close();
   }).catch(err => {
     res.status(500).send({message: '${err}'})
@@ -41,8 +47,10 @@ exports.getErrors = (req, res) => {
     return pool.request().query('select count(*) as \'errors\' from tb_cfdi_error')
   }).then(result => {
     let rows = result.recordset
-    console.log(rows);
-    res.status(200).json(rows)
+    res.status(200).json({
+      title: 'pendientes',
+      quantity: rows[0].errors
+    })
     sql.close();
   }).catch(err => {
     res.status(500).send({message: '${err}'})
